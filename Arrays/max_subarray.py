f9d2b34ba4fa3038f344094e22ef908cecf3faa4
Akshaya Sanga <sangaakshaya7@gmail.com>
2026-05-10 21:03:50 +0530

    Add maxSubArray function for maximum subarray sum
    Implement Kadane's algorithm to find the maximum subarray sum.

---
+++ Arrays/max_subarray.py
@@ -0,0 +1,10 @@
+def maxSubArray(nums):
+    max_sum = nums[0]
+    curr_sum = nums[0]
+    for i in range(1, len(nums)):
+        curr_sum = max(nums[i], curr_sum + nums[i])
+        max_sum = max(max_sum, curr_sum)
+    return max_sum
+
+# Example run
+print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))  # Output: 6
