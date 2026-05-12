fcb519feef104d09bceece708c71d20c2cd46f46
Akshaya Sanga <sangaakshaya7@gmail.com>
2026-05-12 19:38:30 +0530

    Update valid_parentheses_optimized.py

--- StacksQueues/valid_parentheses_optimized.py
+++ StacksQueues/valid_parentheses_optimized.py
@@ -1,1 +1,13 @@
+def isValidOptimized(s:str)->bool:
+    stack=[]; mapping={')':'(',']':'[','}':'{'}
+    for ch in s:
+        if ch in mapping.values(): stack.append(ch)
+        elif ch in mapping.keys():
+            if stack and stack[-1]==mapping[ch]: stack.pop()
+            else: return False
+    return not stack
+
+if __name__=="__main__":
+    print(isValidOptimized("()[]{}")) # True
+    print(isValidOptimized("(]"))     # False
 
