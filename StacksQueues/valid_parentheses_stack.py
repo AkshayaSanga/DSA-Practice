fca6fef18d85e27efb0a8abe7b661cf5c04786b0
Akshaya Sanga <sangaakshaya7@gmail.com>
2026-05-12 19:38:09 +0530

    Update valid_parentheses_stack.py

--- StacksQueues/valid_parentheses_stack.py
+++ StacksQueues/valid_parentheses_stack.py
@@ -1,1 +1,15 @@
+def isValid(s:str)->bool:
+    stack=[]
+    for ch in s:
+        if ch in "({[": stack.append(ch)
+        else:
+            if not stack: return False
+            top=stack.pop()
+            if (ch==")" and top!="(") or (ch=="]" and top!="[") or (ch=="}" and top!="{"):
+                return False
+    return not stack
+
+if __name__=="__main__":
+    print(isValid("({[]})")) # True
+    print(isValid("([)]"))   # False
 
