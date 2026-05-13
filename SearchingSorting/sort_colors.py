e6c3e678fb15941d7e49bed013a61d390267fdd7
Akshaya Sanga <sangaakshaya7@gmail.com>
2026-05-13 18:31:00 +0530

    Add sortColors function to sort an array of colors
    Implement Dutch National Flag algorithm to sort colors.

--- SearchingSorting/sort_colors.py
+++ SearchingSorting/sort_colors.py
@@ -1,1 +1,13 @@
+# LeetCode 75: Sort Colors
+def sortColors(nums):
+    low, mid, high = 0, 0, len(nums)-1
+    while mid <= high:
+        if nums[mid] == 0:
+            nums[low], nums[mid] = nums[mid], nums[low]
+            low+=1; mid+=1
+        elif nums[mid] == 1:
+            mid+=1
+        else:
+            nums[mid], nums[high] = nums[high], nums[mid]
+            high-=1
 
